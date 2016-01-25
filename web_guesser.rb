53d81100318b28e6eaf22afd305753841ac03e89
Adam Hundley <adamhundley@gmail.com>
2016-01-24 22:09:52 -0700

    added color functionality

--- web_guesser.rb
+++ web_guesser.rb
@@ -11,19 +11,36 @@ def check_guess(guess)
     <br>
     The SECRET NUMBER was #{SECRET_NUMBER}"
   elsif guess > (SECRET_NUMBER + 5)
-      "WAY too high!"
+    "WAY too high!"
   elsif guess > SECRET_NUMBER
-      "Too high!"
+    "Too high!"
   elsif guess < (SECRET_NUMBER - 5)
     "WAY too low!"
   elsif guess < SECRET_NUMBER
-      "Too Low!"
+    "Too Low!"
   end
 end
+
+def color_options(guess)
+  if guess == SECRET_NUMBER
+    "Green"
+  elsif guess > (SECRET_NUMBER + 5) || guess < (SECRET_NUMBER - 5)
+    "Red"
+  elsif guess > SECRET_NUMBER || guess < SECRET_NUMBER
+    "Pink"
+  end
+end
+
+
+def catching_cheaters
+
+end
+
 
 get '/' do
   guess = params['guess'].to_i
   message = check_guess(guess)
+  background_color = color_options(guess)
 
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :background_color => background_color}
 end
