cae7af62f97ebd40c1c95e0a2a3b1b1eb7853a32
Adam Hundley <adamhundley@gmail.com>
2016-01-24 22:24:33 -0700

    added cheat functionality

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,7 +3,6 @@ require 'sinatra/reloader'
 
 SECRET_NUMBER = rand(100)
 
-
 def check_guess(guess)
   if guess == SECRET_NUMBER
     "You got it right!
@@ -28,18 +27,23 @@ def color_options(guess)
     "Red"
   elsif guess > SECRET_NUMBER || guess < SECRET_NUMBER
     "Pink"
+  else
+    "White"
   end
 end
 
-
 def catching_cheaters
-
+  "CHEATER!
+  <br>
+  <br>
+  The SECRET NUMBER is #{SECRET_NUMBER}"
 end
 
 
 get '/' do
   guess = params['guess'].to_i
   message = check_guess(guess)
+  message = catching_cheaters if params['cheat'] == 'true'
   background_color = color_options(guess)
 
   erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :background_color => background_color}
